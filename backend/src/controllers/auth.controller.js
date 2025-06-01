6c587165db80930826d80c236046dcb3a12707e7
ronik297 <ronik.allen@gmail.com>
2025-06-01 13:51:31 +0530

    added update profile, auth middleware, check profile route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateJWTToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
@@ -110,4 +111,43 @@ export const logout = (req, res) => {
     } catch (error) {
         
     }
+}
+
+export const updateProfile = async (req, res) => {
+   try {
+     const { profilePic } =  req.body;
+     const user =- req.user._id ;
+
+     if(!profilePic) {
+         return res.status(400).json({
+             message: "Profile picture is required"
+         });
+     }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+        user,
+        { profilePic: uploadResponse.secure_url },
+        { new: true }
+    );
+
+    res.status(200).json(updatedUser)
+
+   } catch (error) {
+         console.error("Error in updateProfile controller", error);
+         res.status(500).json({
+            message: "Internal Server Error"
+         });
+   }
+}
+
+export const checkAuth = (req, res) => {
+   try {
+        res.status(200).json(req.user);
+   } catch (error) {
+        console.error("Error in checkAuth controller", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+   }
 }
