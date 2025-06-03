5ca8cb53af19bdd5d4c9ba203d7479ecad3e3c62
ronik297 <ronik.allen@gmail.com>
2025-06-03 17:43:43 +0530

    deployment setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +23,13 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    }
+    );
+}
 
 server.listen(PORT, () => {
     connectDB();
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import type { AxiosError } from 'axios';
 import { io, Socket } from 'socket.io-client';
 
-const BASE_URL = 'http://localhost:5001';
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/";
 
 type SignupData = {
     email: string;
